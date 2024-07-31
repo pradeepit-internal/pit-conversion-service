af1fd4591bfd48129bef6613162418bddf7bac7c
Sanjeevpradeepit <154218692+Sanjeevpradeepit@users.noreply.github.com>
2024-07-31 12:36:47 +0530

    remove-node-module

--- node_modules/docx/build/file/styles/defaults/run-properties.d.ts
+++
@@ -1,6 +0,0 @@
-import { IRunStylePropertiesOptions } from "../../../file/paragraph/run/properties";
-import { XmlComponent } from "../../../file/xml-components";
-export declare class RunPropertiesDefaults extends XmlComponent {
-    private readonly properties;
-    constructor(options?: IRunStylePropertiesOptions);
-}
